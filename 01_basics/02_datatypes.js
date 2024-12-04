b02e71a164c8fcad341928bae44b20fb48d15667
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-04 13:36:12 +0000

    container config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+// data types
+
+//Primitive data types:-
+// number => 2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>the value is not defined   
+//symbol=> to define the uniqueness
+
+//objects
