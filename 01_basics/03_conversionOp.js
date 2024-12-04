56df1d67414f4d775e42e0d8e0c90914d744204f
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-04 15:32:38 +0000

    container config

--- 01_basics/03_conversionOp.js
+++ 01_basics/03_conversionOp.js
@@ -16,7 +16,18 @@ let str2 = " Apurva"
 let str3 = str1+str2
 console.log(str3);
 
-console.log(1+"2")
-console.log("1"+2)
-console.log(1+2+"2")
-console.log("1"+2+2)
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log(1+2+"2")
+// console.log("1"+2+2)
+// console.log(3+4*5%6);
+console.log(+true);
+let a = 2
+const b = ++a
+let c = 4
+const d = c++
+const e = c++
+
+console.log(b);
+console.log(e);
+
