8c93da11e966e0a5c0f6dd0d651ddac8c7907fb7
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-04 13:27:43 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14435
+let accountEmail = "apurva@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gujarat"
+
+// accountId = 2
+
+accountEmail = "appu@gmail.com"
+accountPassword = "345345"
+accountCity = "Surat"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
