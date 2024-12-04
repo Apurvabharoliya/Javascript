0dbf1d74b4dc9442520ed45ff95c2ead7c4aa3c1
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-04 15:00:39 +0000

    container config

--- 01_basics/03_conversionOp.js
+++ 01_basics/03_conversionOp.js
@@ -1,5 +1,22 @@
-let score = 33
+// let score = 33
 
-console.log(typeof score);
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+//************************************Operations*********************************//
+
+let value = 5
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Apurva"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log(1+"2")
+console.log("1"+2)
+console.log(1+2+"2")
+console.log("1"+2+2)
