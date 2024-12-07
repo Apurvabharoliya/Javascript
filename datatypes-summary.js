6374659159e514129d876a6e2ce06fdccf9d07c9
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-07 03:32:12 +0000

    container config

---
+++ datatypes-summary.js
@@ -0,0 +1,17 @@
+// Primitive
+// 7 types: String,Number,Boolean,NULL,undefined,Symbol,BigInt
+// Primitive data types are also known as call by value
+
+// Non Primitive
+// Arrays,Objects,Function
+// Non primitive data type is also known as call by reference 
+
+const score = 100
+const scoreValue = 100.3
+
+// the both values above are considered as number
+
+const isLoggedIn = false    //boolean
+const outsideTemp = null    //null
+let userEmail;  //undefined
+const id = Symbol('123')
