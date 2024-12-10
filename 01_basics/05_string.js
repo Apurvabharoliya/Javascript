8d001bc8a7898a02d6025d3922634be2ea1ab497
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-10 04:03:59 +0000

    container config

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,6 +1,13 @@
-const name = "Apurva"
-const repoCount = 12
+// const name = "Apurva"
+// const repoCount = 12
 
-console.log(name + repoCount);  //Don't print string like this
+// console.log(name + repoCount);  //Don't print string like this
 
-console.log(`Hello ${name} your repo count is ${repoCount}`);   //Print string like this
+// console.log(`Hello ${name} your repo count is ${repoCount}`);   //Print string like this
+
+const name = new String('Apurva')
+
+console.log(name[0]);
+console.log(name.__proto__);
+
+console.log(name.length);
