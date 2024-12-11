06abd38a00174184669d954a8d0e9faf34065497
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-11 10:00:12 +0000

    container config

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,7 +1,31 @@
+// +++++++++++++++++++++++++++++++++++ nums +++++++++++++++++++++++++++++++++++++++++++++
 const score = 400
-console.log(score);
+// console.log(score);
 const balance = new Number(100)
-console.log(balance);
-console.log(balance.toString())
-console.log(balance.toFixed(4));
-console.log(balance.toString().length);
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toFixed(4));
+// console.log(balance.toString().length);
+
+const otherNumber = 123.8769
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(5.8))
+// console.log(Math.ceil(5.8))
+// console.log(Math.floor(5.8));
+// console.log(Math.min(5,4,3,2,1));
+// console.log(Math.max(4,6,2,1,7));
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max-min+1))+min);
