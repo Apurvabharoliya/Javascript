8b9a0c3717bcb56e82b4fb8551a956e8fa531256
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-12 10:28:54 +0000

    container config

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,14 @@
+// Dates
+
+let myDates = new Date()
+// console.log(myDates)
+// console.log(myDates.toDateString())
+// console.log(myDates.toISOString())
+// console.log(myDates.toJSON())
+// console.log(myDates.toLocaleDateString())
+// console.log(myDates.toLocaleString())
+
+let myCreatDates = new Date(2025, 0, 12)
+let mydates = new Date(2024, 1, 12)
+console.log(myCreatDates.toDateString());
+console.log(mydates.toDateString());
