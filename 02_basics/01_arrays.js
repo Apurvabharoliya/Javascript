167de8ef6fd2d10e9742c72b151f6d17e45936eb
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-12 16:02:04 +0000

    container config

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,6 @@
 //arrays
 
-const myArr = [12,23,45,56,566,7]
+const myArr = [0,1,2,3,4,5,6,7]
 // const myHeros = ["Iron Man","Miles Moralis","Spider-man"]
 // const myarray = new Array(1,2,3,4,5)
 // console.log(myArr[4]);
@@ -10,4 +10,16 @@ const myArr = [12,23,45,56,566,7]
 // myArr.push(34)
 // myArr.pop()
 // myArr.unshift(23.67)
-console.log(myArr);
+// myArr.shift()
+// const newarr = myArr.join()
+// console.log(myArr);
+// console.log(newarr);
+
+//slice, splice
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
