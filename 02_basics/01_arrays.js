0601fb1d1d4da4479436dee4f2511180f5f0cb61
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-12 15:47:44 +0000

    container config

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,13 @@
 //arrays
 
-let myArr = [12,23,45,56,566,7]
-console.log(myArr[4]);
+const myArr = [12,23,45,56,566,7]
+// const myHeros = ["Iron Man","Miles Moralis","Spider-man"]
+// const myarray = new Array(1,2,3,4,5)
+// console.log(myArr[4]);
+
+//arrays methods
+// myArr.push(10)
+// myArr.push(34)
+// myArr.pop()
+// myArr.unshift(23.67)
+console.log(myArr);
