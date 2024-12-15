66968cf5a1426f728ce37b409abdd5a2f8808936
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-15 14:55:23 +0000

    container config

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,12 @@
+//singleton
+//object.create
+
+// Object literals
+const JsUsers = {
+    name: "Apurva",
+    age: 19,
+    place: "Surat",
+    email: "apurva@gmail.com",
+    isLoggedin: false,
+    lastLoggedIn: ["Monday","Tuesday"]
+}
