f75814a571b808fa26e50b76fe285c7345c1155d
Apurvabharoliya <129653669+Apurvabharoliya@users.noreply.github.com>
2024-12-15 15:03:03 +0000

    container config

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,11 +2,19 @@
 //object.create
 
 // Object literals
+const mySym = Symbol("key1")
+
 const JsUsers = {
     name: "Apurva",
+    "Full name": "Apurva Bharoliya",
     age: 19,
     place: "Surat",
     email: "apurva@gmail.com",
     isLoggedin: false,
     lastLoggedIn: ["Monday","Tuesday"]
 }
+
+console.log(JsUsers.email)
+console.log(JsUsers["age"])
+//Use square brackets('[]') because you don't know that the keys of the objects have space in between them or not, So use square brackets mostly.
+console.log(JsUsers["Full name"])
